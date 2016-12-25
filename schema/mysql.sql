a0eae05d1d64e87f82dd61ab196f19f4c5abe8e2
Dmitry Khlystov <dkhlystov@gmail.com>
2016-12-25 17:21:36 +1000

    full text added to schema

--- schema/mysql.sql
+++ schema/mysql.sql
@@ -2,10 +2,13 @@ create table if not exists `News`
 (
 	`id` int(10) not null auto_increment,
 	`active` tinyint(1) default 1,
+	`alias` varchar(100) default null,
 	`date` datetime default null,
 	`title` varchar(100) default null,
+	`preview` text,
 	`text` text,
 	`url` varchar(200) default null,
 	primary key (`id`),
+	key `alias` (`alias`),
 	key `date` (`active`,`date`)
 ) engine InnoDB;
