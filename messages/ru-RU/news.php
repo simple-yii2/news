4c8f5e30a3e18f093874e2e6551d41c3277bcbea
Dmitry Khlystov <dkhlystov@gmail.com>
2016-12-25 14:27:47 +1000

    ru translation added

---
+++ messages/ru-RU/news.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+	'News' => 'Новости',
+	'Create' => 'Добавить',
+	'Create news' => 'Добавить новость',
+
+	'Changes saved successfully.' => 'Изменения успешно сохранены.',
+	'Item not found.' => 'Объект не найден.',
+	'Item deleted successfully.' => 'Объект успешно удален.',
+
+	'Active' => 'Активный',
+	'Date' => 'Дата',
+	'Title' => 'Заголовок',
+	'Text' => 'Текст',
+	'Url' => 'Ссылка',
+
+	'Save' => 'Сохранить',
+	'Cancel' => 'Отмена',
+];
